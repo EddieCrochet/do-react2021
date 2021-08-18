c077a8f09b0c12231f8b236e4854393a861064e8
Eddie Crochet <eddiecrochet1994@gmail.com>
2021-08-18 15:57:40 -0500

    working with user input, state, two way binding, and child-to-parent component communication

--- 01-starting-setup/src/components/Expenses/NewExpense/NewExpense.js
+++ 01-starting-setup/src/components/Expenses/NewExpense/NewExpense.js
@@ -2,10 +2,22 @@ import React from 'react';
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
-const NewExpense = () => {
-    return <div className="new-expense">
-        <ExpenseForm />
+const NewExpense = (props) => {
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        };
+        props.onAddExpense(expenseData);
+    };
+
+    return (
+    <div className="new-expense">
+         {/*this sets a pointer to the saveExpenseDataHandler function 
+         in this ExpenseForm instance*/}
+        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
+    );
 };
 
 export default NewExpense;
