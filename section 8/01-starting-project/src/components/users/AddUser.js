b56e70f3edffe939e90afe89a14d0694a5cf9869
Eddie Crochet <eddiecrochet1994@gmail.com>
2021-12-21 13:43:03 -0600

    mnaged the state of the error by adding functionality to click on backdrop or ok button to exit error message

--- section 8/01-starting-project/src/components/users/AddUser.js
+++ section 8/01-starting-project/src/components/users/AddUser.js
@@ -7,13 +7,22 @@ import ErrorModal from '../UI/ErrorModal';
 const AddUser = (props) => {
     const [enteredUsername, setEnteredUsername] = useState('');
     const [enteredAge, setEnteredAge] = useState('');
+    const [error, setError] = useState();
 
     const addUserHandler = (event) => {
         event.preventDefault();
         if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+            setError({
+                title: 'Invalid input',
+                message: 'Please enter a valid name and age (non-empty values).'
+            });
             return;
         }
-        if(+enteredAge <= 0) {
+        if(+enteredAge < 1) {
+            setError({
+                title: 'Invalid age',
+                message: 'Please enter a valid age greater than 0.'
+            });
             return;
         }
         props.onAddUser(enteredUsername, enteredAge);
@@ -30,9 +39,18 @@ const AddUser = (props) => {
         setEnteredAge(e.target.value);
     };
 
+    const errorHandler = () => {
+        setError(null);
+    }
+
     return (
         <div>
-            <ErrorModal title="An error ocurred!" message="Something went wrong..."/>
+            {/* referring to below --> error is undefined until it exists... this is how we
+            conditionally render a jsx element */}
+            {error && <ErrorModal 
+                title={error.title} 
+                message={error.message}
+                onConfirm={errorHandler}/>}
             <Card className={classes.input}>
                 <form onSubmit={addUserHandler}>
                     <label htmlFor="username">Username</label>
