29c7bdaeb5513399e73f7cfdcc22b5244ca1e3dc
Eddie Crochet <eddiecrochet1994@gmail.com>
2022-01-11 15:03:13 -0600

    notes, and playing with and understanding refs

--- section 8/01-starting-project/src/components/users/AddUser.js
+++ section 8/01-starting-project/src/components/users/AddUser.js
@@ -1,4 +1,4 @@
-import React, {useState, Fragment} from 'react';
+import React, {useState, Fragment, useRef} from 'react';
 import Card from '../UI/Card';
 import classes from './AddUser.module.css';
 import Button from '../UI/Button';
@@ -6,39 +6,57 @@ import Wrapper from '../Helpers/Wrapper';
 import ErrorModal from '../UI/ErrorModal';
 
 const AddUser = (props) => {
-    const [enteredUsername, setEnteredUsername] = useState('');
-    const [enteredAge, setEnteredAge] = useState('');
+    const nameInputRef = useRef();
+    const ageInputRef = useRef();
+
+    // now using refs to listen for values, so dont need to listen 
+    // for every keystroke in state
+    // I know this is technically an awful place to implement refs but I just wanna play
+    // refs are best when I want to read a value and not use it
+
+    // const [enteredUsername, setEnteredUsername] = useState('');
+    // const [enteredAge, setEnteredAge] = useState('');
     const [error, setError] = useState();
 
     const addUserHandler = (event) => {
         event.preventDefault();
-        if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+        const enteredName = nameInputRef.current.value;
+        const enteredUserAge = ageInputRef.current.value;   
+        if(enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
             setError({
                 title: 'Invalid input',
                 message: 'Please enter a valid name and age (non-empty values).'
             });
             return;
         }
-        if(+enteredAge < 1) {
+        // convert to a number and com pare if less than 1
+        if(+enteredUserAge < 1) {
             setError({
                 title: 'Invalid age',
                 message: 'Please enter a valid age greater than 0.'
             });
             return;
         }
-        props.onAddUser(enteredUsername, enteredAge);
+        // passing AddUser state back down to the App
+        props.onAddUser(enteredName, enteredUserAge);
         //console.log(enteredUsername, enteredAge);
-        setEnteredAge('');
-        setEnteredUsername('');
+
+        // setEnteredAge('');
+        // setEnteredUsername('');
+
+        nameInputRef.current.value = '';
+        ageInputRef.current.value = '';
     };
 
-    const usernameChangeHandler = (e) => {
-        setEnteredUsername(e.target.value);
-    };
+    //state handlers - once again I've changed to using refs for this portion
 
-    const ageChangeHandler = (e) => {
-        setEnteredAge(e.target.value);
-    };
+    // const usernameChangeHandler = (e) => {
+    //     setEnteredUsername(e.target.value);
+    // };
+
+    // const ageChangeHandler = (e) => {
+    //     setEnteredAge(e.target.value);
+    // };
 
     const errorHandler = () => {
         setError(null);
@@ -59,14 +77,16 @@ const AddUser = (props) => {
                     <input 
                         id="username" 
                         type="text" 
-                        onChange={usernameChangeHandler}
-                        value={enteredUsername}/>
+                        // onChange={usernameChangeHandler}
+                        // value={enteredUsername}
+                        ref={nameInputRef}/>
                     <label htmlFor="age">Age (Years)</label>
                     <input 
                         id="age" 
                         type="number" 
-                        onChange={ageChangeHandler}
-                        value={enteredAge}/>
+                        // onChange={ageChangeHandler}
+                        // value={enteredAge}
+                        ref={ageInputRef}/>
                     <Button type="submit">Add User</Button>
                 </form>
             </Card>
