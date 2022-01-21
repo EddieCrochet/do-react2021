93cf0e5a3064c4a884c5805271b3d079edc954f7
Eddie Crochet <eddiecrochet1994@gmail.com>
2022-01-21 14:20:40 -0600

    added an auth context to handle the global isLoggedIn state

--- 06-usereducer-starting-project/src/App.js
+++ 06-usereducer-starting-project/src/App.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './contextStore/auth-context';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -28,13 +29,19 @@ function App() {
   };
 
   return (
-    <React.Fragment>
+    //  Technically don't need the fragment since the 
+    // auth can be used as root component
+    //<React.Fragment>
+      <AuthContext.Provider value={{
+        isLoggedIn: isLoggedIn
+      }}>
       <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+      </AuthContext.Provider>
+    //</React.Fragment>
   );
 }
 
