c42221cbf58b66b9cbd2ade42c69970d130e947f
Eddie Crochet <eddiecrochet1994@gmail.com>
2022-02-02 22:46:42 -0600

    added fthe cart button and began functionality for the Meals component  with dummy data in a local array for now

--- section11/01-starting-project/src/components/Layout/Header.js
+++ section11/01-starting-project/src/components/Layout/Header.js
@@ -1,5 +1,5 @@
 import {Fragment} from 'react';
-
+import HeaderCartButton from './HeaderCartButton';
 import mealImg from '../../assets/meals.jpg';
 import classes from './Header.module.css';
 
@@ -8,7 +8,7 @@ const Header = props => {
      <Fragment>
          <header className={classes.header}>
              <h1>Meals with React!</h1>
-             <button>Cart</button>
+             <HeaderCartButton/>
          </header>
          <div className={classes["main-image"]}>
              <img src={mealImg} alt='a table full of varius delicious foods!'/>
