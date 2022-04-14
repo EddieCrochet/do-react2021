0a4994c3711281283b8448b5bef47fa57ae35749
Eddie Crochet <eddiecrochet1994@gmail.com>
2022-04-14 16:58:19 -0500

    added a header cart button

--- section11/01-starting-project/src/components/Layout/HeaderCartButton.js
+++ section11/01-starting-project/src/components/Layout/HeaderCartButton.js
@@ -1,7 +1,16 @@
 import CartIcon from "../Cart/CartIcon";
 import classes from './HeaderCartButton.module.css';
 
+import { useContext } from 'react';
+import cartContext from "../../store/cart-context";
+
 const HeaderCartButton = props => {
+    const cartCtx = useContext(cartContext);
+
+    const numberOfItems = cartCtx.items.reduce((curNum, item) => {
+        return curNum + item.amount;
+    }, 0);
+
     return (
         <button className={classes.button} onClick={props.onCluck}>
             <span className={classes.icon}>
